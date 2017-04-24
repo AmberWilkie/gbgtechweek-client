cb9c2da9fa41b65de945f2174deaca4219a6f802
Amber <amber@amberwilkie.com>
2017-04-24 16:52:53 +0200

    Include proper styling and html for client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  data = {};
+  private apiUrl = 'https://address-book-demo.herokuapp.com/api/contacts';
+
+  constructor(private http: Http) {
+    this.getContacts().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
+  }
+
+  getContacts() {
+    return this.http.get(this.apiUrl)
+      .map((res: Response) => res.json());
+  }
 }
